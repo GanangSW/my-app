871f68affe95e192cf8d5c8aec6585e5b30ae17b
Ganang Samudra Wibawa <ganangsamudra1@gmail.com>
2022-02-26 10:10:02 +0700

    upload deploy script

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:GanangSW/my-app.git master:gh-pages
+git push -f git@github.com:GanangSW/my-app.git origin:gh-pages
 
 cd -
